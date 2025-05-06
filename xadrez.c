57058a1431761a07ffbc95a615736939821a06b1
Camila da Rosa <53013110+Camila-R@users.noreply.github.com>
2025-05-06 13:45:56 +0000

    nivelIntermediario

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,7 @@ int main(){
     int casaRainha = 1;
     int casaTorre = 1;
     int casaBispo = 1;
+    int casaCavaloL = 1;
 
     //Rainha
     // movimento da Rainha oito casas para a esquerda
@@ -14,6 +15,8 @@ int main(){
         casaRainha++;
     }
     
+    printf("\n");
+
     //Torre
     // movimento da Torre cinco casas para a direita
     printf("Movimento Torre \n");
@@ -22,14 +25,31 @@ int main(){
 
        casaTorre++;
     } while (casaTorre <= 5);
+
+    printf("\n");
     
     //Bispo
     // movimento do Bispo cinco casas na diagonal para cima e à direita
     printf("Movimento Bispo \n");
     for (casaBispo = 1; casaBispo <= 5; casaBispo++){
         printf("Cima, Direita \n");
+    };
+
+    printf("\n");
+
+    //Cavalo
+    // movimento do Cavalo duas casas para baixo e uma casa para a esquerda
+    printf("Movimento do Cavalo \n");
+    while (casaCavaloL > 0){
+
+        for (int casaCavaloHV = 1; casaCavaloHV <= 2; casaCavaloHV++){
+            printf("Baixo \n");
+        }
+        
+        printf("Esquerda \n");
+        casaCavaloL--;
+        
     }
     
-
     return 0;
 }
